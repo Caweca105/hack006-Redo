4403d5b915b99d7c4eff89207b7b2e302d407683
jnassula <jonatanassula@gmail.com>
2021-07-21 22:49:10 +0100

    building routes

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from '../styles/Landingpage.module.css'
+import Navbar from './Navbar'
 
     function LandingPage({ pagina }) {
-            return   <div className={styles.Landingpage}>
+            return (
+                <div className={styles.Landingpage}>
                     <img className={styles.img3} src="/images/landingpage.png"/>
-                </div>
+                    
+                </div> 
+            )  
         };
         
 
