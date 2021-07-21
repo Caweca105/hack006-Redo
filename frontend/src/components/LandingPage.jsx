3b8b480914adc907847fa00584edbffbc029c112
tiagodfcosta <83545729+tiagodfcosta@users.noreply.github.com>
2021-07-21 21:05:05 +0100

    rebased

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar';
 import styles from '../styles/Landingpage.module.css'
+
 
     function LandingPage({ pagina }) {
             return   <div className={styles.Landingpage}>
+                     <Navbar/>
                     <img className={styles.img3} src="/images/landingpage.png"/>
                 </div>
         };
