fee65bc67f4cb9540af782ccffa52746794d655c
jnassula <jonatanassula@gmail.com>
2021-07-21 11:58:46 +0100

    added components and styles

---
+++ frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import styles from '../styles/App.module.css'
+
+function App() {
+  return (
+    <div className={styles.App.module.css}>
+      
+    </div>
+  );
+}
+
+export default App;
