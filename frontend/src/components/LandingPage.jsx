98d03bd39d5ef3b25b0b40b9ae26822a23b5c117
tiagodfcosta <83545729+tiagodfcosta@users.noreply.github.com>
2021-07-21 17:55:29 +0100

    Landing page

---
+++ frontend/src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/Landingpage.module.css'
+
+    function LandingPage({ pagina }) {
+            return   <div className={styles.Landingpage}>
+                    <img className={styles.img3} src="/images/landingpage.png"/>
+                </div>
+        };
+        
+
+        
+
+
+export default LandingPage
