58307d1d628b86f6a55dc65638c0751976501508
jnassula <jonatanassula@gmail.com>
2021-07-22 14:42:02 +0100

    added navbar

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from './Navbar'
 import styles from '../styles/Landingpage.module.css'
 
 
@@ -7,9 +8,7 @@ import styles from '../styles/Landingpage.module.css'
     function LandingPage({ pagina }) {
         return (
             <>
-                <div className={styles.Navbar}>
-                    <img src="/images/sinta_logo.png" />
-                </div>
+                <Navbar />
                 <div className={styles.Landingpage}>
                     <img className={styles.img3} src="/images/landingpage.png"/>
                 </div> 
