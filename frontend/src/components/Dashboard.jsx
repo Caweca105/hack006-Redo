24f52ed263a60530172b39e7f10f79a7df0c650c
jnassula <jonatanassula@gmail.com>
2021-07-22 14:29:17 +0100

    interface of dahsboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -7,8 +7,8 @@ function Dashboard() {
 
   const [bootcamp, setBootcamps] = useState([])
   useEffect(() => {
-    
-    //setBootcamps([])
+
+    setBootcamps()
     
   }, []);
   return (
@@ -16,20 +16,26 @@ function Dashboard() {
       <Navbar />
       <div className={styles.Container}>
         <div  className={styles.Profile}>
-          <Link to="/profile">Perfil</Link>
-        </div>
-        <div className={styles.ContainerMain}>
-          <div className={styles.Main}>Conteudo</div>
-          <div className={styles.Support}>
-            <form action="https://www.seg-social.pt/inicio" target="_blank">
-              <input type="image" value="Seg Social" src="/images/segsocial.png" />
-            </form>
-            <form action="https://www.portaldasfinancas.gov.pt/at/html/index.html" target="_blank">
-              <input type="image" value="Portal das Finanças" src="/images/financas.png"/>
-            </form>
-            <form action="https://iefponline.iefp.pt/IEFP/index2.jsp" target="_blank">
-              <input type="image" value="IEFP" src="/images/IEFP.png"/>
-            </form>
+          <Link to="/profile"><h2>Meu Perfil</h2></Link>
+          <div>
+            <img src="/images/avatar.png" />
+          </div>
+          <div>
+            <h3>João Miguel</h3>
+          </div>
+          <div>
+            <h4>Sobre mim</h4>
+            <p>Casado, 44 anos, desempregado.
+              Em busca de novos desafios profissionais.
+            </p>
+          </div>
+          <div>
+            <h4>Habilitações Literárias</h4>
+            <p>Bacharel em Recursos Humanos </p>
+          </div>
+          <div>
+            <h4>Áreas de interesse</h4>
+          <p>Informática, Manutenção, Culinária</p>
           </div>
         </div>
         {/* {
@@ -50,48 +56,90 @@ function Dashboard() {
           })
         } */}
         <div className={styles.Content}>
+          
           <div className={styles.ContentTitle}>
             <Link to="/bootcamp"><h2>Bootcamps</h2></Link> 
-            {
-              bootcamps.map((bootcamp, index) => {
-                return (
-                  <Link to={`/bootcamp/${bootcamp._id}`}>
-                    <div className={styles.Bootcamp}>
-                      <div className={styles.card}>
-                        <img src="/images/lecapsule_js.png" />
-                        <div className={styles.container}>
-                          <h4>{bootcamp.name}</h4>
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
-
-                )
-              })
-            }
+            <div className={styles.Bootcamp}>
+              <div className={styles.card}>
+                <img src="/images/lecapsule_js.png" />
+                <div className={styles.container}>
+                  <h4>Le capsule</h4>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <img src="/images/b4f_js.png" />
+                <div className={styles.container}>
+                  <h4>Bytes4Future</h4>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <img src="/images/ironhack_js.jpg" />
+                <div className={styles.container}>
+                  <h4>Ironhack</h4>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <img src="/images/42_js.jpg" />
+                <div className={styles.container}>
+                  <h4>42 Lisboa</h4>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <img src="/images/academia_js.jpg" />
+                <div className={styles.container}>
+                  <h4>Academia do Código</h4>
+                </div>
+              </div>
+            </div>
           </div>
-        
           <div className={styles.ContentTitle}>
-            <Link to="/bootcamp"><h2>Empregos</h2></Link> 
-            {
-              jobs.map((jobs, index) => {
-                return (
-                  <Link to={`/bootcamp/${jobs._id}`}>
-                    <div className={styles.Bootcamp}>
-                      <div className={styles.card}>
-                        <img src="/images/lecapsule_js.png" />
-                        <div className={styles.container}>
-                          <h4>{jobs.name}</h4>
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
+            <Link to="/jobs"><h2>Empregos</h2></Link>
+            <div className={styles.Jobs}>
+              <div className={styles.card}>
+                <img src="/images/sapo_js.png" />
+                <div className={styles.container}>
+                  <h4>Sapo Empregos</h4>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <img src="/images/adecco_js.png" />
+                <div className={styles.container}>
+                  <h4>Adecco</h4>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <img src="/images/indeed_js.png" />
+                <div className={styles.container}>
+                  <h4>Indeed</h4>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <img src="/images/netempregos_js.jpg" />
+                <div className={styles.container}>
+                  <h4>Net-Empregos</h4>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <img src="/images/iefp_js.png" />
+                <div className={styles.container}>
+                  <h4>IEFP</h4>
+                </div>
+              </div>
 
-                )
-              })
-            }
+            </div>
           </div>
-        </div>  
+          <div className={styles.Support}>
+            <form action="https://www.seg-social.pt/inicio" target="_blank">
+              <input type="image" value="Seg Social" src="/images/segsocial.png" />
+            </form>
+            <form action="https://www.portaldasfinancas.gov.pt/at/html/index.html" target="_blank">
+              <input type="image" value="Portal das Finanças" src="/images/financas.png"/>
+            </form>
+            <form action="https://iefponline.iefp.pt/IEFP/index2.jsp" target="_blank">
+              <input type="image" value="IEFP" src="/images/IEFP.png"/>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
