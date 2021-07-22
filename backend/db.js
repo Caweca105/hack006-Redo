8baad42721ae13e0fd4318a37f30dbe67661b83e
João <joaopedro.bn@gmail.com>
2021-07-22 10:59:03 +0100

    backend e mongoDB

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,43 @@
-import mongodb from 'mongodb'
+const { MongoClient } = require("mongodb")
 
-export async function insertBootcamp(products) {
-    const collection = await getCollection(DB_REDO, "bootcamps");
-    const res = await collection.insertMany(products);
-    return res;
+const URI = "mongodb://localhost:27017"
+let client;
+
+async function connect (uri) {
+    try {
+        if(client) return client;
+        client = new MongoClient (uri, {
+            usedUnifiedTopology: true
+        })
+        await client.connect();
+
+        return client;
+    } catch (err) {
+        console.log(`[ERRO]: ${err}`)
+    }
+}
+
+async function getCollection(dbName, collectionName) {
+    const client = await connect(URI);
+    const db = client.db(dbName)
+    const collection = db.collection(collectionName)
+    return collection
+}
+
+async function sendJobsToDb(data, dbName, collectionName) {
+    const collection = await getCollection(dbName, collectionName)
+    const result = await collection.insertMany(data[collectionName])
+    return result
+}
+
+async function sendBootcampsToDb(data, dbName, collectionName) {
+    const collection = await getCollection(dbName, collectionName)
+    const result = await collection.insertMany(data[collectionName])
+    return result
+}
+
+module.exports = {
+    getCollection,
+    sendBootcampsToDb,
+    sendJobsToDb
 }
